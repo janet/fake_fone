506238e76f4c354ae2bdf4e758a14d6b84f3f7e1
janet <jskenmotsu@gmail.com>
2017-07-29 11:22:11 -0700

    sends texts

--- server.py
+++ server.py
@@ -1,7 +1,28 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect
+from twilio_client import client
+import os
+
 app = Flask(__name__)
+app.secret_key = os.environ['SECRET_KEY']
+
+
 
 
 @app.route('/')
-def hello_world():
-    return 'Hello, World!'
+def main():
+    return render_template('main.html')
+
+
+@app.route('/send_text', methods=['POST'])
+def send_text():
+
+    phone = request.form.get("phone")
+    text = request.form.get("message")
+
+    message = client.api.account.messages.create(
+        to="+15106840288",
+        from_="+15103451264",
+        body=text)
+
+    return redirect('/')
+
